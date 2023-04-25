3f6d3bd6df6624b69b4283085d7ba54f240659b0
William Lindholm <a22willi@student.his.se>
2023-04-25 15:50:53 +0200

    Created RecyclerViewAdapter.

---
+++ app/src/main/java/com/example/networking/RecyclerViewAdapter.java
@@ -0,0 +1,50 @@
+package com.example.networking;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.MyViewHolder> {
+
+    private List<Mountain> mountains;
+    private Context context;
+
+    public RecyclerViewAdapter(Context context, List<Mountain> mountains) {
+        this.context = context;
+        this.mountains = mountains;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerViewAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.mountain_item, parent, false);
+        return new RecyclerViewAdapter.MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerViewAdapter.MyViewHolder holder, int position) {
+        holder.name.setText(mountains.get(position).getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mountains.size();
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+
+        TextView name;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
