8e702573cba5681cb470bca2569b86b257bc1f2c
William Lindholm <a22willi@student.his.se>
2023-05-02 10:18:44 +0200

    Now using JsonURL to fetch data from famouspeaks.

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -17,7 +17,7 @@ import java.util.List;
 @SuppressWarnings("FieldCanBeLocal")
 public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
 
-    private final String JSON_URL = "HTTPS_URL_TO_JSON_DATA_CHANGE_THIS_URL";
+    private final String JSON_URL = "https://mobprog.webug.se/json-api?login=brom";
     private final String JSON_FILE = "mountains.json";
 
     @Override
@@ -25,7 +25,7 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        new JsonFile(this, this).execute(JSON_FILE);
+        new JsonTask( this).execute(JSON_URL);
     }
 
     @Override
@@ -46,5 +46,7 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
 
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        adapter.notifyDataSetChanged();
     }
 }
